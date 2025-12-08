eeca4f0ef913a4491729d8f34d483e0fec6216f4
nandosolis15-max <nandosolis15@icloud.com>
2025-12-08 19:35:33 +0000

    Loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,19 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# # Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# # While loops  
+
+# for x in reverse(range(1, 11)):
+#     print(x)
+
+# Print("happy new years")
+
+name = input("enter your name: ")
+
+while name == "":
+    print("YOU DID NOT ENTER YOUR NAME")
+    name = input ("Enter your name: ")
+print(f"Hello {name}")
+
+age = int(input("Enter your age: ")
+        
+while age < 0:
+    print
