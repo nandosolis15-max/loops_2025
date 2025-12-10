c71157ea9d65dba3fb8ee370c3f4401bf694addf
nandosolis15-max <nandosolis15@icloud.com>
2025-12-10 19:02:32 +0000

    total

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,27 +1,27 @@
-# # # Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-# # # While loops  
+# # # # Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# # # # While loops  
 
-# # for x in reverse(range(1, 11)):
-# #     print(x)
+# # # for x in reverse(range(1, 11)):
+# # #     print(x)
 
-# # Print("happy new years")
+# # # Print("happy new years")
 
-# name = input("enter your name: ")
+# # name = input("enter your name: ")
 
-# while name == "":
-#     print("YOU DID NOT ENTER YOUR NAME")
-#     name = input ("Enter your name: ")
-# print(f"Hello {name}")
+# # while name == "":
+# #     print("YOU DID NOT ENTER YOUR NAME")
+# #     name = input ("Enter your name: ")
+# # print(f"Hello {name}")
 
-# age = int(input("Enter your age: ")
+# # age = int(input("Enter your age: ")
         
-# while age < 0:
-#     print
+# # while age < 0:
+# #     print
 
 
 subjects = ["Math", "Science", "History", "Art"]
-for subject in subjects: 
-    print(subjects)
+# for subject in subjects: 
+#     print(subjects)
 
 
 
@@ -37,16 +37,30 @@ for subject in subjects:
     print (subject)
 
 
-list1000 = list(range(1, 1000))
+# list1000 = list(range(1, 1000))
+
+# # for number in list1000:
+# #     if number > 599:
+# #         break
+# #     print(number)
 
 # for number in list1000:
-#     if number > 599:
+#     if 300 < number < 599:
 #         break
 #     print(number)
 
-for number in list1000:
-    if 300 < number < 599:
-        break
-    print(number)
+for index in range(len(subjects)):
+    print("Subject " + str(index) + ": " + subjects[index])
 
 
+numbers =[5, 10, 15, 20]
+total = 0
+for number in numbers:
+    total += number
+    print("total: ", total)
+
+new_numbers = list(range(1,261))
+total = 0
+for number in new_numbers:
+    total += number
+print("total sum frm 1 to 260 ", total)
