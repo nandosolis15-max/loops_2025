a3916869f5291dc4f059a02c977f19e88cb66262
nandosolis15-max <nandosolis15@icloud.com>
2025-12-10 18:38:20 +0000

    Zips

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,19 +1,52 @@
-# # Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-# # While loops  
+# # # Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# # # While loops  
 
-# for x in reverse(range(1, 11)):
-#     print(x)
+# # for x in reverse(range(1, 11)):
+# #     print(x)
 
-# Print("happy new years")
+# # Print("happy new years")
 
-name = input("enter your name: ")
+# name = input("enter your name: ")
 
-while name == "":
-    print("YOU DID NOT ENTER YOUR NAME")
-    name = input ("Enter your name: ")
-print(f"Hello {name}")
+# while name == "":
+#     print("YOU DID NOT ENTER YOUR NAME")
+#     name = input ("Enter your name: ")
+# print(f"Hello {name}")
 
-age = int(input("Enter your age: ")
+# age = int(input("Enter your age: ")
         
-while age < 0:
-    print
+# while age < 0:
+#     print
+
+
+subjects = ["Math", "Science", "History", "Art"]
+for subject in subjects: 
+    print(subjects)
+
+
+
+for subject in subjects:
+    if subjects == "History":
+        break
+    print(subjects)
+
+
+for subject in subjects:
+    if subject == "Science":
+        continue
+    print (subject)
+
+
+list1000 = list(range(1, 1000))
+
+# for number in list1000:
+#     if number > 599:
+#         break
+#     print(number)
+
+for number in list1000:
+    if 300 < number < 599:
+        break
+    print(number)
+
+
